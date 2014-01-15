1b3a6fe1e3e23eccf78ce5bbb62e1fd21cbf375e
Robert Chan <robertchan@Robert-Chans-MacBook-Air.local>
2014-01-15 09:51:35 -0800

    Adding OSX driver and scripts

---
+++ src/com/test/TestLoginPage_Chrome_OSX.java
@@ -0,0 +1,59 @@
+package com.test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import com.LoginPage;
+
+public class TestLoginPage_Chrome_OSX {
+	
+	private LoginPage page;
+	private WebDriver driver;
+	private String redirectURL = "http://testhomework.wikia.com/wiki/Test-homework_Wiki";
+	private String testUserName = "rochan_test1";
+	private String testPassword = "testing";
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		
+		String path = getClass().getClassLoader().getResource("").getPath();
+		System.out.println(path);
+		System.setProperty("webdriver.chrome.driver", path+"chromedriver");
+		driver = new ChromeDriver();
+		driver.get("http://testhomework.wikia.com/");
+		page = (LoginPage) PageFactory.initElements(driver, LoginPage.class);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.close();
+	}
+	
+	@Test
+	public void testLoginPage() {
+		Assert.assertTrue("The browser redirect URL is not equal to " + redirectURL, redirectURL.equals(driver.getCurrentUrl()));
+	   	Assert.assertFalse("The browser redirect URL should not equal to 'http://testhomework.wikia.com/'", redirectURL.equals("http://testhomework.wikia.com/"));
+	   	page.hoverOverLogin();	
+	   	Assert.assertTrue(page.isUserLoginDropDownVisible());
+	   	page.loginAs(testUserName, testPassword);
+		String displayedName = page.getUserNameLabel();
+		Assert.assertFalse("Label is still showing 'Log In' instead of the username.", displayedName.equals("Log in"));
+    	Assert.assertTrue("Name being displayed is not the same as the user name. Expecting: " + testUserName + ". Actual: " + displayedName, displayedName.equals(testUserName));
+	}
+}
